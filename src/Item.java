582435cb6151a97bff90508ccfe0f36cc3762b8c
Danii1Ka1inichev <Ka1inichevDanii1@yandex.ru>
2022-06-07 17:33:13 +0300

    Добавлен первый релиз

---
+++ src/Item.java
@@ -0,0 +1,12 @@
+public class Item {
+
+    String name;
+    int price;
+    int quantity;
+
+    Item(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
